704e6d1eb8baebc954f8cb8ee03d124364d8555f
Francisco Colombo <franciscocolombounlp@gmail.com>
2026-01-29 21:39:07 -0300

    Agrego carpeta styles con archivos scss vacios y modifique un poco la estructura inicial de la app.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,17 +3,25 @@ import Carrito from "./CartWidget"
 function NavBar(){
     return (
         <div>
-            <h1>iSolutions</h1>
+            <h1 className="brand-name">iSolutions</h1>
+            <span className="full-name">Francisco Colombo</span>
             <nav className="navBar-container">
                 <ul className="navBar-list">
                     <li><a href="">Home</a></li>
                     <li><a href="">Sobre mi</a></li>
                     <li><a href="">Port Folio</a></li>
                     <li><a href="">Preguntas frecuentes</a></li>
+                    <li><a href="">Paquetes</a></li>
                     <li><a href="">Contacto</a></li>
-                    <li><Carrito /></li>
+                    <li><a href=""><Carrito /></a></li>
                 </ul>
             </nav>
+            <p>
+                Transformo tus ideas en soluciones robustas y te doy la libertad para dominarlas.
+            </p>
+            <div className="button-container">
+                <button className="button">Conoce mi Propuesta</button>
+            </div>
         </div>
     );
 }
