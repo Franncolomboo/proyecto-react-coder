20ac2718d2c2c4da58990a9def3e551b1a6ac37d
Francisco Colombo <franciscocolombounlp@gmail.com>
2026-01-31 16:15:38 -0300

    CreaTuLanding+Colombo

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import NavBar  from "./components/NavBar" 
 import ItemListContainer from "./components/ItemListContainer"
+import './style.scss'
+
 
 function App() {
   const mensaje = "Bienvenidos a iSolutions";
